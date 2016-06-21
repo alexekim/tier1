3247badef1fcea2369087f04b1cf67f2fc40e30a
Alex Kim <alexander.e.kim@gmail.com>
2016-06-21 13:32:00 -0500

    added rspec

---
+++ tier1.rb
@@ -0,0 +1,14 @@
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,2]
+
+
+def find_duplicate(array)
+  array.detect{ |value| array.count(value) > 1 }
+end
+
+p find_duplicate(a)
+
+def scramble(array)
+	array = array.shuffle
+	return array
+end
+p scramble(a)
